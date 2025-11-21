03e9e1b9cea1babe717a1458a3faaa2883b295d7
Mayra <mayra.bf3@gmail.com>
2025-11-21 20:15:47 -0300

    Feat: Agrega tests de transformaciones

---
+++ test/ar/edu/unlam/pb2/criaturasElementales/AscensoDelVientoTest.java
@@ -0,0 +1,18 @@
+package ar.edu.unlam.pb2.criaturasElementales;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AscensoDelVientoTest {
+
+    @Test
+    public void queCambieAfinidadAAire() {
+        Criatura base = new CriaturaDomesticada("Leaf", 80, Afinidad.TIERRA);
+
+        Criatura transformada = new AscensoDelViento(base);
+
+        assertEquals(Afinidad.AIRE, transformada.getAfinidad());
+    }
+
+}
